1f94b7e777f418712c25e48e0bdcd787e4a8a7ba
Praveen Kumar Shanmugam <spraveen@cs.utah.edu>
2014-04-14 15:58:38 -0600

    Bug fixes for faster location updates using wifi and gps with higher accuracy.

--- wifiApFinder/src/com/example/wifiapfinder/WifiApFinderPhase1.java
+++ wifiApFinder/src/com/example/wifiapfinder/WifiApFinderPhase1.java
@@ -272,11 +272,11 @@ public class WifiApFinderPhase1 extends Activity {
 		double longitude; // longitude
 
 		// The minimum distance to change Updates in meters
-		private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1; // 10
-																		// meters
+		private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 0; // always
 
 		// The minimum time between updates in milliseconds
-		private static final long MIN_TIME_BW_UPDATES = (long) (1000 * 60 * 0.1); // 10
+		private static final long MIN_TIME_BW_UPDATES = (long) (1000 * 10); // 10
+																			// seconds
 		// seconds
 
 		// Declaring a Location Manager
@@ -293,8 +293,6 @@ public class WifiApFinderPhase1 extends Activity {
 				// getting network status
 				isNetworkEnabled = locationManager
 						.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-				// disable network location
-				isNetworkEnabled = false;
 
 				if (!isGPSEnabled && !isNetworkEnabled) {
 					// no network provider is enabled
@@ -308,28 +306,30 @@ public class WifiApFinderPhase1 extends Activity {
 								MIN_TIME_BW_UPDATES,
 								(float) MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
 						Log.d("Network", "Network");
-						// always update the location
-						location = locationManager
-								.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-						if (location != null) {
-							latitude = location.getLatitude();
-							longitude = location.getLongitude();
+						if (location == null) {
+							location = locationManager
+									.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+							if (location != null) {
+								latitude = location.getLatitude();
+								longitude = location.getLongitude();
+							}
 						}
 					}
 					// if GPS Enabled get lat/long using GPS Services
 					if (isGPSEnabled) {
-						// always update the location
-						locationManager.requestLocationUpdates(
-								LocationManager.GPS_PROVIDER,
-								MIN_TIME_BW_UPDATES,
-								MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-						Log.d("GPS Enabled", "GPS Enabled");
-						if (locationManager != null) {
-							location = locationManager
-									.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-							if (location != null) {
-								latitude = location.getLatitude();
-								longitude = location.getLongitude();
+						if (location == null) {
+							locationManager.requestLocationUpdates(
+									LocationManager.GPS_PROVIDER,
+									MIN_TIME_BW_UPDATES,
+									MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+							Log.d("GPS Enabled", "GPS Enabled");
+							if (locationManager != null) {
+								location = locationManager
+										.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+								if (location != null) {
+									latitude = location.getLatitude();
+									longitude = location.getLongitude();
+								}
 							}
 						}
 					}
@@ -344,7 +344,9 @@ public class WifiApFinderPhase1 extends Activity {
 
 		@Override
 		public void onLocationChanged(Location location) {
-
+			this.location = location;
+			Toast.makeText(getApplicationContext(), "GPS location updated"+location,
+					Toast.LENGTH_SHORT).show();
 		}
 
 		@Override
